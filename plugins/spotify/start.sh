dc01d5bda238365779227dd6ba52e287260d50e5
ReservedEhlek <p.wrzecionowski@gmail.com>
2023-11-19 17:09:03 +0100

    new file:   .env 	modified:   docker-compose.yml 	new file:   error.log 	renamed:    plugins/spotify/Dockerfile.template -> plugins/spotify/Dockerfile 	modified:   plugins/spotify/start.sh

--- plugins/spotify/start.sh
+++ plugins/spotify/start.sh
@@ -9,7 +9,9 @@ fi
 # SOUND_DEVICE_NAME: Set the device broadcast name for Spotify
 # SOUND_SPOTIFY_BITRATE: Set the playback bitrate
 SOUND_DEVICE_NAME=${SOUND_DEVICE_NAME:-"balenaSound Spotify $(echo "$BALENA_DEVICE_UUID" | cut -c -4)"}
-SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-160}
+SOUND_SPOTIFY_BITRATE=${SOUND_SPOTIFY_BITRATE:-320}
+SOUND_SPOTIFY_DISABLE_NORMALISATION=DISABLE
+SOUND_SPOTIFY_ENABLE_CACHE=ENABLE
 
 # SOUND_SPOTIFY_DISABLE_NORMALISATION: Disable volume normalization
 if [[ -z ${SOUND_SPOTIFY_DISABLE_NORMALISATION+x} ]]; then
@@ -40,11 +42,17 @@ echo "Device name: $SOUND_DEVICE_NAME"
 [[ -n ${SOUND_SPOTIFY_ENABLE_CACHE+x} ]] && echo "Spotify audio cache enabled."
 
 set -- /usr/bin/librespot \
-  --backend pulseaudio \
+  --backend pipe \
   --name "$SOUND_DEVICE_NAME" \
-  --bitrate "$SOUND_SPOTIFY_BITRATE" \
   --cache /var/cache/raspotify \
   --volume-ctrl linear \
+  --initial-volume=30 \
+  -v \
+  | pacat --latency-msec=20
+#  --bitrate "$SOUND_SPOTIFY_BITRATE" \
+#  --format S32 \
+#  --dither none \
+
   "$@"
 
 exec "$@"
